53564da1b27fc42180115002d216a7cfe6de87bc
VAISNAWI <vaisnawiraja@gmail.com>
2025-05-29 08:33:10 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!-- <?php 
+    session_start(); 
+    ?> --> 
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head> 
+        <meta charset="UTF-8"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Sistem E-Booking Bilik Pentadbiran | ILPS</title> 
+        <!-- bootstrap css --> 
+        <!-- <link rel="stylesheet" type="text/css" href="DataTables\css\dataTables.bootstrap.min.css"> --> 
+        <link rel="stylesheet" href="bootstrap\css\bootstrap.min.css"> 
+        <link rel="stylesheet" href="DataTables\css\dataTables.bootstrap5.min.css"> 
+        <!-- bootstrap icon --> 
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">    
+    </head> 
+    <style>
+  h1 {
+  color: #256D85;
+  text-shadow: 2px 2px 4px #000000;
+  font-family:'Lucida Handwriting';
+}
+  </style>
+    <body> 
+    <img src="img/banner.png" style="width: 100%">
+    <div class="container">
+<br>
+<center><h1> WELCOME TO OUR WEBSITE </h1></center>
+<br>
+<tr height='150'bgcolor='#FFFFFF'>
+    <td>
+    
+    <table width='90%'>
+    <tr align='center'>
+    <td width='25%'>
+    <a href='user.php'>
+            <img src='img/user.png'></a>
+        </td>
+
+        <td width='14%'>
+            <a href='login_admin.php'>
+            <img src='img/admin.jpg'></a>
+        </td>
+    <tr align='center'>
+<td>STAFF ILPS</td>
+<td>ADMIN ILPS</td>
+</tr>
+</table>
+</div>
+<br><br>
+<br><br>
+ <!-- footer -->
+ <footer>
+                 <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            <p class="text-black">  Copyright © 2022 : E-BOOKING BILIK PENTADBIRAN SISTEM ILP Selandar</p>
+            </div>
+            </footer>
+
+</body>
+</html>
+
+
+
